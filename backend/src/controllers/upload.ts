4bb5f07a611414384ee7f81f9281fda366badcbc
yurydzal <yurydzal@yandex.ru>
2025-09-14 16:01:05 +0400

    review 14

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -1,7 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import { constants } from 'http2'
 import { join } from 'path'
+import sharp from 'sharp'
 import BadRequestError from '../errors/bad-request-error'
+
+const MIN_FILE_SIZE = 2 * 1024
 
 export const uploadFile = async (
     req: Request,
@@ -11,6 +14,17 @@ export const uploadFile = async (
     if (!req.file) {
         return next(new BadRequestError('Файл не загружен'))
     }
+
+    if (req.file.size < MIN_FILE_SIZE) {
+        return next(new BadRequestError('Слишком маленький файл'))
+    }
+
+    try {
+        await sharp(req.file.path).metadata()
+    } catch (error) {
+        return next(new BadRequestError('Неверный формат изображения'))
+    }
+
     try {
         const fileName = process.env.UPLOAD_PATH
             ? join('/', process.env.UPLOAD_PATH, req.file.filename)
