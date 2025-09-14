1ee250b4ac3b3f6e7807438ed78cbc3f76dae679
yurydzal <yurydzal@yandex.ru>
2025-09-14 14:48:50 +0400

    review 9

--- backend/src/controllers/upload.ts
+++ backend/src/controllers/upload.ts
@@ -11,9 +11,13 @@ export const uploadFile = async (
         return next(new BadRequestError('Файл не загружен'))
     }
     try {
+        const fileName = process.env.UPLOAD_PATH
+            ? `/${process.env.UPLOAD_PATH}/${req.file.filename}`
+            : `/${req.file.filename}`
+
         return res.status(constants.HTTP_STATUS_CREATED).send({
-            fileName: req.file.filename,
-            originalName: req.file?.originalname,
+            fileName,
+            originalName: req.file.originalname,
         })
     } catch (error) {
         return next(error)
